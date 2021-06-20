a212c0a2d9ea9878494b51513a5ffd6a8a90e1e1
saddam-maratouq <sadam.sadam.sadam@hotmail.com>
2021-06-20 22:46:00 +0300

    food pro

---
+++ app.js
@@ -0,0 +1,26 @@
+let userName=  prompt("What's your name ? ");
+console.log(userName)
+
+alert("welcome " + userName )
+
+
+let age= prompt( ' how do you have ')
+
+console.log( age )
+
+alert("Nice to meet you" ) 
+
+
+let s= prompt( ' whats your favourite food ')
+
+console.log( s)
+
+alert(' nice choice  ' + userName)
+
+
+let x= prompt( ' do you like fast food which one ')
+ console.log( x)
+
+ alert (' warning its not good all time ')
+
+
